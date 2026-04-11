8d227302db08ae1af96d0293b220b3f25faf402d
XiaoshiTwinkling <1730578@qq.com>
2026-04-11 13:59:52 +0800

    同时穿戴猫耳和猫尾的时候 会获得迅捷一

--- src/main/java/com/nekotech/item/custom/TailItem.java
+++ src/main/java/com/nekotech/item/custom/TailItem.java
@@ -2,7 +2,10 @@ package com.nekotech.item.custom;
 
 import com.mojang.serialization.Codec;
 import com.nekotech.item.ModItem;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
@@ -56,5 +59,73 @@ public class TailItem extends ModItem implements Equipment {
         public String asString() {
             return this.name;
         }
+    }
+
+    /**
+     * 检查玩家是否同时装备了猫尾和猫耳
+     * @param player 要检查的玩家
+     * @return 如果同时装备了猫尾和猫耳则返回 true
+     */
+    public static boolean hasTailAndEars(PlayerEntity player) {
+        if (player == null) {
+            return false;
+        }
+
+        // 检查玩家是否装备了猫尾
+        ItemStack tailStack = player.getEquippedStack(EquipmentSlot.LEGS);
+        boolean hasTail = !tailStack.isEmpty() && tailStack.getItem() instanceof TailItem;
+
+        // 检查玩家是否装备了猫耳
+        ItemStack hatStack = player.getEquippedStack(EquipmentSlot.HEAD);
+        boolean hasEars = !hatStack.isEmpty() &&
+                (hatStack.getItem().getTranslationKey().equals("item.neko-technology.neko_ears") ||
+                        hatStack.getItem() instanceof com.nekotech.item.custom.HatItem);
+
+        return hasTail && hasEars;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (!world.isClient() && entity instanceof PlayerEntity player) {
+            if (slot >= 0 && slot < 4 && isEquippedByPlayer(stack, player)) {
+                if (hasTailAndEars(player)) {
+
+                    StatusEffectInstance speedEffect = new StatusEffectInstance(StatusEffects.SPEED, 100, 0, false, false, true);
+
+                    // 检查是否已经有迅捷效果，如果没有则添加
+                    if (!player.hasStatusEffect(StatusEffects.SPEED)) {
+                        player.addStatusEffect(speedEffect);
+                    } else {
+                        // 如果已有迅捷效果，确保持续时间是足够的
+                        StatusEffectInstance existingEffect = player.getStatusEffect(StatusEffects.SPEED);
+                        if (existingEffect != null && existingEffect.getDuration() < 50) {
+                            // 如果效果持续时间快结束了，刷新它
+                            player.addStatusEffect(speedEffect);
+                        }
+                    }
+                } else if (player.hasStatusEffect(StatusEffects.SPEED)) {
+                    StatusEffectInstance effect = player.getStatusEffect(StatusEffects.SPEED);
+                    if (effect != null && effect.getDuration() <= 100 + 20) {
+                    }
+                }
+            }
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    /**
+     * 检查这个物品是否被指定玩家装备
+     * @param stack 要检查的物品堆栈
+     * @param player 玩家实体
+     * @return 如果物品被玩家装备返回 true
+     */
+    private boolean isEquippedByPlayer(ItemStack stack, PlayerEntity player) {
+        // 检查各个装备槽位
+        for (ItemStack armorStack : player.getArmorItems()) {
+            if (ItemStack.areItemsEqual(armorStack, stack)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
